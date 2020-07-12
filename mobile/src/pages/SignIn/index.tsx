8cf076c57d8a59d14f23cd9e9cc71796e3f6048a
wvcneto <wvcneto@gmail.com>
2020-07-12 03:24:57 -0300

    views signIn/signUp and components/fonts

---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Faça seu login</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('click!');
+        }}
+      >
+        Entrar
+      </Button>
+    </Container>
+  );
+};
+
+export default SignIn;
